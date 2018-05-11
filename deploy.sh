2afa2cbbbc2aa391c0d271cb77a7aee782cbbba8
Kristofer <kris@drtorte.net>
2018-05-11 12:46:51 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ curl -s -X POST \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    -H "Travis-API-Version: 3" \
-   -H "Authorization: token xxxxxx" \
+   -H "Authorization: token $token" \
    -d "$body" \
    https://api.travis-ci.com/repo/DrTorteTwo%2Fddemo/requests
